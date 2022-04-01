c4766a53e1b701fe55ebc2d6caa05829485aeb0f
Nonumery <nonum.work@gmail.com>
2022-04-01 16:12:54 +0300

    changed status codes

--- core/errors.py
+++ core/errors.py
@@ -1,11 +1,11 @@
 from fastapi import HTTPException, status
 
-TOKEN_EXC = HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Invalid auth token")
+TOKEN_EXC = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid auth token")
 AUTH_EXC = HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
 CRED_EXC = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Credentials are not valid")
 LANG_EXC = HTTPException(status_code=status.HTTP_306_RESERVED, detail="Language doesn't exist")
 NAME_EXC = HTTPException(status_code=status.HTTP_306_RESERVED, detail="Track name isn't avaluable")
 RECORD_EXC = HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="Record is not found")
-ACCESS_EXC = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No access")
+ACCESS_EXC = HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="No access")
 EMAIL_EXC = HTTPException(status_code=status.HTTP_306_RESERVED, detail="Email is already used")
-PASS_EXC = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Wrong password")
+PASS_EXC = HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Wrong password")
