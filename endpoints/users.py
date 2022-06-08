72c51539d4330e33abd00c2b8288aed643a6da93
root <root@HOME-PC.localdomain>
2022-06-09 02:33:03 +0300

    --

--- endpoints/users.py
+++ endpoints/users.py
@@ -13,21 +13,23 @@ from repositories.users import UserRepository
 from .depends import get_current_user, get_session, get_user_email, get_user_repository
 from models.users import User, UserChange, UserIn
 from sqlalchemy.ext.asyncio import AsyncSession
-router = APIRouter()  
-    
-@router.get("/", response_model=List[User], response_model_exclude=["hash_password", "update_token"])
+router = APIRouter()
+
+
+@router.get("/", response_model=List[User], response_model_exclude=["hash_password", "update_token"], include_in_schema=False)
 async def read_users(
-    users: UserRepository = Depends(get_user_repository),
-    session : AsyncSession = Depends(get_session),
-    limit: int = 10, 
-    skip: int = 0):
+        users: UserRepository = Depends(get_user_repository),
+        session: AsyncSession = Depends(get_session),
+        limit: int = 10,
+        skip: int = 0):
     return await users.get_users(session=session, limit=limit, skip=skip)
+
 
 @router.post("/")
 async def create_new_user(
-    user: UserIn,
-    users: UserRepository = Depends(get_user_repository),
-    session : AsyncSession = Depends(get_session)):
+        user: UserIn,
+        users: UserRepository = Depends(get_user_repository),
+        session: AsyncSession = Depends(get_session)):
     c_user = await users.get_user_by_email(session, user.email)
     token = create_access_token({"sub": user.email})
     if not c_user:
@@ -44,19 +46,20 @@ async def create_new_user(
     text = f'To confirm email, follow the link: {MAIN_URL}/users/registration/{user.email}/{user.password}/{token}'
     confirm_email(to=user.email, text=text)
 
-@router.get("/registration/{email}/{password}/{token}", response_model=User, response_model_exclude=["hash_password", "update_token"])
+
+@router.get("/registration/{email}/{password}/{token}", response_model=User, response_model_exclude=["hash_password", "update_token"], include_in_schema=False)
 async def confirm_user_email(
-    email: str,
-    password : str,
-    token: str,
-    users: UserRepository = Depends(get_user_repository),
-    session : AsyncSession = Depends(get_session)):
+        email: str,
+        password: str,
+        token: str,
+        users: UserRepository = Depends(get_user_repository),
+        session: AsyncSession = Depends(get_session)):
     n_email = get_user_email(token)
     if n_email == email:
         c_user = await users.get_user_by_email(session, email)
         if c_user.update_token != token:
             raise TOKEN_EXC
-        #Пароль можно не проверять
+        # Пароль можно не проверять
         if not verify_password(password, c_user.hash_password):
             raise PASS_EXC
         token = create_access_token({"sub": email})
@@ -67,12 +70,13 @@ async def confirm_user_email(
                 os.mkdir(user_folder)
             return new_user
 
+
 @router.patch("/", response_model=User, response_model_exclude=["hash_password", "update_token"])
 async def update_user(
-    user: UserChange,
-    users: UserRepository = Depends(get_user_repository),
-    current_user: User = Depends(get_current_user),
-    session : AsyncSession = Depends(get_session)):
+        user: UserChange,
+        users: UserRepository = Depends(get_user_repository),
+        current_user: User = Depends(get_current_user),
+        session: AsyncSession = Depends(get_session)):
     if current_user.email_confirm == False:
         raise CONFIRM_EXC
     if not verify_password(user.password, current_user.hash_password):
@@ -83,11 +87,12 @@ async def update_user(
 
     return await users.update_user(session, int(current_user.id), user.email, user.new_password)
 
+
 @router.get("/recovery/{email}")
 async def recover_user(
-    email: str,
-    users: UserRepository = Depends(get_user_repository),
-    session : AsyncSession = Depends(get_session)):
+        email: str,
+        users: UserRepository = Depends(get_user_repository),
+        session: AsyncSession = Depends(get_session)):
     user = await users.get_user_by_email(session=session, email=email)
     if user:
         token = create_access_token({"sub": user.email})
@@ -99,16 +104,16 @@ async def recover_user(
 
 @router.get("/recovery/{email}/{token}")
 async def recover_pass(
-    email: str,
-    token: str,
-    users: UserRepository = Depends(get_user_repository),
-    session : AsyncSession = Depends(get_session)):
-    user : User = await get_current_user(users, session, token)
+        email: str,
+        token: str,
+        users: UserRepository = Depends(get_user_repository),
+        session: AsyncSession = Depends(get_session)):
+    user: User = await get_current_user(users, session, token)
     if user.email == email:
         new_password = secrets.token_urlsafe(10)
         new_password = new_password[:9]
         await users.update_user(session, int(user.id), user.email, hashlib.sha256(new_password.encode('utf-8')).hexdigest())
-        #await users.update_user(session, int(user.id), user.email, new_password)
+        # await users.update_user(session, int(user.id), user.email, new_password)
         text = f'New password: {new_password}'
         password_recovery(to=email, text=text)
     else:
@@ -117,7 +122,7 @@ async def recover_pass(
 # @router.delete("/")
 
 # async def delete_current_user(
-    
+
 #     user_id:int,
 #     users : UserRepository = Depends(get_user_repository),
 #     session : AsyncSession = Depends(get_session)
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 import os
 import shutil
 from fastapi import Depends, FastAPI, Request, Response, staticfiles, status
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse, FileResponse
 from httpcore import URL
 from pytest import Session
 import uvicorn
-from core.config import INSTALL_APP
+from core.config import IMAGE_FORMAT, INSTALL_APP
 from db.tables import Base, engine, LanguageTable
 from endpoints import users, auth, tracks, wellknown
 from sqlalchemy import insert
@@ -17,17 +17,18 @@ from endpoints.depends import get_session, get_track_repository
 
 from repositories.tracks import TrackRepository
 
+
 async def init_models():
     async with engine.begin() as conn:
-        #await conn.run_sync(Base.metadata.drop_all)
-        #if (os.path.isdir("tracks")):
-            #shutil.rmtree("tracks", ignore_errors=False, onerror=None)
+        # await conn.run_sync(Base.metadata.drop_all)
+        # if (os.path.isdir("tracks")):
+        #shutil.rmtree("tracks", ignore_errors=False, onerror=None)
         await conn.run_sync(Base.metadata.create_all)
         if not (os.path.isdir("tracks")):
             os.mkdir("tracks")
         try:
-            await conn.execute(insert(LanguageTable).values(id = 0, language="rus"))
-            await conn.execute(insert(LanguageTable).values(id = 1, language="eng"))
+            await conn.execute(insert(LanguageTable).values(id=0, language="rus"))
+            await conn.execute(insert(LanguageTable).values(id=1, language="eng"))
         except(Exception):
             print("Already exists")
 
@@ -36,7 +37,16 @@ app.include_router(router=users.router, prefix="/users", tags=["users"])
 app.include_router(router=auth.router, prefix="/auth", tags=["auth"])
 app.include_router(router=tracks.router, prefix="/tracks", tags=["tracks"])
 #app.include_router(router=wellknown.router, prefix="/.well-known", tags=["well-known"])
-@app.get("/")
+# app.mount("/resources",
+#           staticfiles.StaticFiles(directory="resources"), name="resources")
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse('resources/logo.ico')
+
+
+@app.get("/", include_in_schema=False)
 def main():
     return {"status": "ok"}
 # app.mount("/tracks_", staticfiles.StaticFiles(directory="tracks"), name="tracks")
@@ -44,7 +54,7 @@ def main():
 # @app.get("/audios")
 
 # async def get_tracks(
-    
+
 #     user_id: int
 # ):
 #     out = []
@@ -55,16 +65,16 @@ def main():
 #         })
 #     return out
 
+
 cli = typer.Typer()
+
 
 @cli.command()
 def db_init_models():
     asyncio.run(init_models())
     print("Done")
-    uvicorn.run("main:app", port = 8000, host="0.0.0.0", reload=True)
-
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
 
 
 if __name__ == "__main__":
     asyncio.run(cli())
-    
